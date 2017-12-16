b282286ca2190ce9d7f9e0a8237a17f7bda3927a
Tran Hoan <tr.hoan00@gmail.com>
2017-12-16 08:17:16 +0700

    ignore crash when closing

--- lib/amqp/channel.ex
+++ lib/amqp/channel.ex
@@ -1,32 +1,41 @@
 defmodule AMQP.Channel do
-  @moduledoc """
-  Functions to operate on Channels.
-  """
+    @moduledoc """
+    Functions to operate on Channels.
+    """
 
-  alias AMQP.{Connection, Channel}
+    alias AMQP.{Connection, Channel}
 
-  defstruct [:conn, :pid]
-  @type t :: %Channel{conn: Connection.t, pid: pid}
+    defstruct [:conn, :pid]
+    @type t :: %Channel{conn: Connection.t, pid: pid}
 
-  @doc """
-  Opens a new Channel in a previously opened Connection.
-  """
-  @spec open(Connection.t) :: {:ok, Channel.t} | any
-  def open(%Connection{pid: pid} = conn) do
-    case :amqp_connection.open_channel(pid) do
-      {:ok, chan_pid} -> {:ok, %Channel{conn: conn, pid: chan_pid}}
-      error           -> error
+    @doc """
+    Opens a new Channel in a previously opened Connection.
+    """
+    @spec open(Connection.t) :: {:ok, Channel.t} | any
+    def open(%Connection{pid: pid} = conn) do
+        case :amqp_connection.open_channel(pid) do
+            {:ok, chan_pid} -> {:ok, %Channel{conn: conn, pid: chan_pid}}
+            error -> error
+        end
     end
-  end
 
-  @doc """
-  Closes an open Channel.
-  """
-  @spec close(Channel.t) :: :ok | {:error, any}
-  def close(%Channel{pid: pid}) do
-    case :amqp_channel.close(pid) do
-      :ok -> :ok
-      error -> {:error, error}
+    @doc """
+    Closes an open Channel.
+    """
+    @spec close(Channel.t) :: :ok | {:error, any}
+    def close(%Channel{pid: pid}) do
+        try do
+            if Process.alive?(pid) do
+                case :amqp_channel.close(pid) do
+                    :ok -> :ok
+                    error -> {:error, error}
+                end
+            else
+                :ok
+            end
+        catch
+        _, _ ->
+            {:error, :dead}
+        end
     end
-  end
 end
